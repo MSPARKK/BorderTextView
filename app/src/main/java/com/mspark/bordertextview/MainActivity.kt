acdf5a42a42f42cc4b653e273de236dd5b355e26
MSPARKK <pms93009@gmail.com>
2024-01-05 15:25:01 +0900

    BorderEditText 추가

--- app/src/main/java/com/mspark/bordertextview/MainActivity.kt
+++ app/src/main/java/com/mspark/bordertextview/MainActivity.kt
@@ -3,6 +3,9 @@ package com.mspark.bordertextview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,10 +15,22 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<BorderTextView>(R.id.programmatic_text_view).apply {
             val strokeColorHex = "#000000"
-            val strokeWidth = 10
+            val strokeWidth = 16
 
             setStrokeColor(strokeColorHex)
             setStrokeWidth(strokeWidth)
+        }
+
+        findViewById<Button>(R.id.test_button).setOnClickListener {
+            findViewById<BorderTextView>(R.id.programmatic_text_view).apply {
+                val strokeColorHex = "#FF9800"
+                val strokeWidth = 20
+
+                setStrokeColor(strokeColorHex)
+                setStrokeWidth(strokeWidth)
+
+                gravity = Gravity.RIGHT or Gravity.CENTER_VERTICAL
+            }
         }
     }
 }
